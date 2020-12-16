784279bd81a7571d708a73385a835d6e1deb5474
DeanDal1000 <dalzelldean@gmail.com>
2020-12-16 14:29:47 +0000

    Added POST Request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+
 import GETRequest from './components/GETRequest';
+import POSTRequest from './components/POSTRequest';
+
 
 function App() {
   return (
     <div className="App">
         <GETRequest />
+        <POSTRequest />
     </div>
   );
 }
